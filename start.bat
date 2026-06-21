a0ae6ce7b0a8e4465285731a132a3e003e58f01d
Kirill Kuznetsov <kka.fullstack@gmail.com>
2026-06-21 09:42:49 +0300

    fix

--- start.bat
+++ start.bat
@@ -1,9 +1,4 @@
 @echo off
 
-set "SINGBOX_DIR=%~dp0"
-if "%SINGBOX_DIR:~-1%"=="\" set "SINGBOX_DIR=%SINGBOX_DIR:~0,-1%"
-
-set SINGBOX_EXE=%SINGBOX_DIR%\sing-box.exe
-set CONFD_DIR=%SINGBOX_DIR%\confd
-
-%SINGBOX_EXE% run -D "%SINGBOX_DIR%" -C "%CONFD_DIR%"
+cd /d "%~dp0"
+sing-box.exe run -D . -C confd
